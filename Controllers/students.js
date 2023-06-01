3a72b224df2b16af2b93448fa73b687a374bf415
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-06-01 16:32:35 -0700

    updated api

--- Controllers/students.js
+++ Controllers/students.js
@@ -4,30 +4,30 @@ import { client } from "../db.js";
 
 export function getAllStudents(req){
     return client
-    .db("Guvi").collection("students")
+    .db("Student").collection("students")
     .find(req.query).toArray()
 }
 
 export function getStudentsById(id){
     return client
-    .db("Guvi").collection("students")
+    .db("Student").collection("students")
     .findOne({_id:new ObjectId(id)})
 }
 
 export function addStudentsData(data){
     return client
-    .db("Guvi").collection("students")
-    .insertOne({data})
+    .db("Student").collection("students")
+    .insertOne(data)
 }
 
 export function UpdateStudentData(id,updatedData){
     return client
-    .db("Guvi").collection("students")
+    .db("Student").collection("students")
     .findOneAndUpdate({_id:new ObjectId(id)},{$set:updatedData})
 }
 
 export function DeleteStudentData(id){
     return client
-    .db("Guvi").collection("students")
+    .db("Student").collection("students")
     .deleteOne({_id:new ObjectId(id)})
 }
