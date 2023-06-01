8d05a71baf45684d87028d596b9ab79ccb867f5b
Raajulogu <rajeshkumarlogu145@gmail.com>
2023-06-01 16:37:58 -0700

    cors update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import  express from "express";
 import { Students_list } from "./rout/students.js";
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config()
 
@@ -9,6 +10,7 @@ let port=process.env.PORT;
 let app=express();
 
 app.use(express.json())
+app.use(cors());
 
 app.use("/students",Students_list)
 
